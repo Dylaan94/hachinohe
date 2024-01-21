ece7a4d2dcd3e3cb02e94170110c39e3e1cbe188
Dylan Brain <dylanbrain@googlemail.com>
2024-01-21 20:57:10 +0900

    update text-panel blade

--- web/app/themes/hachinohe/resources/views/blocks/text-panel.blade.php
+++ web/app/themes/hachinohe/resources/views/blocks/text-panel.blade.php
@@ -4,7 +4,7 @@
     <div class="text-panel-container w-3/4 max-w-screen-lg">
         <h2 class="pb-2 text-2xl font-semibold">{{ get_field('title') }}</h2>
         @foreach (get_field('content') as $item)
-            @if ($item['label'] === '')
+            @if ($item['label'] === '' && $item['value'] === '')
                 <span class="row-nocontent block pb-2"></span>
             @else
                 <span class="row flex flex-col sm:flex-row">
@@ -12,7 +12,7 @@
                         <p class="py-2 font-semibold sm:w-1/6 sm:pb-0 sm:font-normal">{{ $item['label'] }}</p>
                         <p class="py-2 text-gray-800 sm:pb-0">{!! $item['value'] !!}</p>
                     @else
-                        <p class="sm:text-lg py-2 font-semibold sm:w-1/6 sm:pb-0">{{ $item['label'] }}</p>
+                        <p class="py-2 font-semibold sm:w-1/6 sm:pb-0 sm:text-lg">{{ $item['label'] }}</p>
                     @endif
                 </span>
             @endif

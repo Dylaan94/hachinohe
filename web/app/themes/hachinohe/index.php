a982ceff4da71ae3efdc7aa6966116212ba1c294
Dylan Brain <dylanbrain@googlemail.com>
2024-01-30 21:54:51 +0900

    updated font

--- web/app/themes/hachinohe/index.php
+++ web/app/themes/hachinohe/index.php
@@ -1,20 +1,55 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
-  </head>
 
-  <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <?php do_action('get_header'); ?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@200&family=Noto+Serif+JP:wght@200&display=swap" rel="stylesheet">
+  <script>
+    (function(d) {
+      var config = {
+          kitId: 'pnn3aan',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+          h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+      h.className += " wf-loading";
+      tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+      tk.async = true;
+      tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState;
+        if (f || a && a != "complete" && a != "loaded") return;
+        f = true;
+        clearTimeout(t);
+        try {
+          Typekit.load(config)
+        } catch (e) {}
+      };
+      s.parentNode.insertBefore(tk, s)
+    })(document);
+  </script>
+  <?php wp_head(); ?>
+</head>
 
-    <div id="app">
-      <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
-    </div>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <?php do_action('get_header'); ?>
 
-    <?php do_action('get_footer'); ?>
-    <?php wp_footer(); ?>
-  </body>
+  <div id="app">
+    <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
+  </div>
+
+  <?php do_action('get_footer'); ?>
+  <?php wp_footer(); ?>
+</body>
+
 </html>
